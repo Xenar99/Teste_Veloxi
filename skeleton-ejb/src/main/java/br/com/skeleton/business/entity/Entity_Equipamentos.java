3a2c62b6728343bcb6ff8dbf2052a0b94cb758c1
Marcelo Lopes Rodrigues <srxmarcelox@outlook.com>
2024-05-26 19:48:49 -0300

    Att

--- skeleton-ejb/src/main/java/br/com/skeleton/business/entity/Entity_Equipamentos.java
+++ skeleton-ejb/src/main/java/br/com/skeleton/business/entity/Entity_Equipamentos.java
@@ -1,67 +1,114 @@
 package br.com.skeleton.business.entity;
 
-public class Entity_Equipamentos{
-	
-protected String ID;
-protected String Equipamento;
-protected String Descricao;
-protected String DataAquisicao;
-protected String Valor;
-protected String DataValidade;
+import java.io.Serializable;
+import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-public Entity_Equipamentos() {
-	
-}
+@Entity
+@Table(name = "Entity_Equipamentos")
+public class Entity_Equipamentos {
 
+	private static final long serialVersionUID = 1L;
 
-public Entity_Equipamentos(String iD, String equipamento, String descricao, String dataAquisicao, String valor,
-		String dataValidade) {
-	super();
-	ID = iD;
-	Equipamento = equipamento;
-	Descricao = descricao;
-	DataAquisicao = dataAquisicao;
-	Valor = valor;
-	DataValidade = dataValidade;
-	
-}
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	protected Long ID;
 
-public String getID() {
-	return ID;
-}
-public void setID(String iD) {
-	ID = iD;
-}
-public String getEquipamento() {
-	return Equipamento;
-}
-public void setEquipamento(String equipamento) {
-	Equipamento = equipamento;
-}
-public String getDescricao() {
-	return Descricao;
-}
-public void setDescricao(String descricao) {
-	Descricao = descricao;
-}
-public String getDataAquisicao() {
-	return DataAquisicao;
-}
-public void setDataAquisicao(String dataAquisicao) {
-	DataAquisicao = dataAquisicao;
-}
-public String getValor() {
-	return Valor;
-}
-public void setValor(String valor) {
-	Valor = valor;
-}
-public String getDataValidade() {
-	return DataValidade;
-}
-public void setDataValidade(String dataValidade) {
-	DataValidade = dataValidade;
-}
+	protected String Equipamento;
+	protected String Descricao;
+	protected String DataAquisicao;
+	protected String Valor;
+	protected String DataValidade;
+
+	public Entity_Equipamentos() {
+
+	}
+
+	public Entity_Equipamentos(String iD, String equipamento, String descricao, String dataAquisicao, String valor,
+			String dataValidade) {
+		super();
+		Equipamento = equipamento;
+		Descricao = descricao;
+		DataAquisicao = dataAquisicao;
+		Valor = valor;
+		DataValidade = dataValidade;
+
+	}
+
+	public Long getID() {
+		return ID;
+	}
+
+	public void setID(Long iD) {
+		ID = iD;
+	}
+
+	public String getEquipamento() {
+		return Equipamento;
+	}
+
+	public void setEquipamento(String equipamento) {
+		Equipamento = equipamento;
+	}
+
+	public String getDescricao() {
+		return Descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		Descricao = descricao;
+	}
+
+	public String getDataAquisicao() {
+		return DataAquisicao;
+	}
+
+	public void setDataAquisicao(String dataAquisicao) {
+		DataAquisicao = dataAquisicao;
+	}
+
+	public String getValor() {
+		return Valor;
+	}
+
+	public void setValor(String valor) {
+		Valor = valor;
+	}
+
+	public String getDataValidade() {
+		return DataValidade;
+	}
+
+	public void setDataValidade(String dataValidade) {
+		DataValidade = dataValidade;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(ID);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Entity_Equipamentos other = (Entity_Equipamentos) obj;
+		return Objects.equals(ID, other.ID);
+	}
+
+	@Override
+	public String toString() {
+		return "Entity_Equipamentos [ID=" + ID + "]";
+	}
+
 
 }
