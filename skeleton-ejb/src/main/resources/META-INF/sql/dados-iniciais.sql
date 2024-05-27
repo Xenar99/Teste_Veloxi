5bce178631c17a12b0eaeb1e17666ab5dae0732c
Marcelo Lopes Rodrigues <srxmarcelox@outlook.com>
2024-05-27 02:29:28 -0300

    Migração para o MySql, database comunicando

---
+++ skeleton-ejb/src/main/resources/META-INF/sql/dados-iniciais.sql
@@ -0,0 +1,1 @@
+INSERT INTO Entity_Equipamentos (ID, Equipamento,Descricao,DataAquisicao,Valor,DataValidade) VALUES (1, 'Multímetro', 'Multímetro Keysight 34461A', '2023-01-01', '21000', '2024-01-01');
